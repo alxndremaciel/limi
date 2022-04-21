b9672755593537bcdbc00b86fee7a667f9afcd2e
Alexandre Maciel <acmaciel@ufpi.edu.br>
2022-04-21 13:43:08 -0300

    fim da b05

--- limi_main.py
+++ limi_main.py
@@ -14,8 +14,25 @@ def load_program(program_name, program):
 
 def program_scanning(program):
     """Analisar o conteúdo de program e criar registers e commands"""
-    registers = []
+    registers = [0, ]
     commands  = []
+
+    for line in program:
+        tokens = line.split(' ')
+        if tokens[0] == 'R':
+            if len(tokens) == 3:
+                try:
+                    X = int(tokens[1])
+                    V = int(tokens[2])
+                    if X == len(registers):
+                        registers.append(V)
+                    else:
+                        print('Erro: Ordem incorreta da definição dos registradores.')
+
+                except ValueError:
+                    print('Erro: Impossível converter str em int.')
+            else:
+                print('Erro: Problema na definição de registradores.')
 
     return registers, commands
 
@@ -27,6 +44,4 @@ program_name = 'examples/somar.lmp'
 program = generate_program(program_name)
 registers, commands = program_scanning(program)
 registers = run_program(registers, commands)
-
-for each in program:
-    print(each)
+print(registers)
