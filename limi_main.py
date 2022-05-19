4cecb2b382f0f178c759c13ab9b57cdea3cbc759
Alexandre Maciel <acmaciel@ufpi.edu.br>
2022-05-19 00:30:30 -0300

    Fim dos comentarios em limi_main

--- limi_main.py
+++ limi_main.py
@@ -36,6 +36,8 @@ class Command:
                     'Número incorreto de tokes. Tem mais de 3 tokens.']
             error_msg(item, messages)
         
+        # TODO: len(tokens[0]) deve ser igual a 1. Erro caso contrario
+
         # O primeiro token é sempre uma operação
         self.operation = tokens[0]
 
@@ -142,6 +144,8 @@ def analyse_module_calling(item, registers):
     
     '''
     line = item.line
+
+    # Extrai argumentos de line entre parenteses
     arguments = line.split('(')[1].split(')')[0]
     if not arguments:
         messages = [
@@ -149,22 +153,29 @@ def analyse_module_calling(item, registers):
             f'É precisso passar argumentos ao chamar um módulo']
         error_msg(item, messages)
     else:
+        # Separa argumentos para testar se sao validos
         for arg in arguments.split(','):
+            # Testa se argumento pode ser convertido para inteiro
             try:
                 X = int(arg)
             except ValueError:
                 messages = [
                     'Erro na chamada de módulo.',
                     f'Argumentos do módulo devem ser inteiros separados por vírgula']
-                error_msg(item, messages)  
+                error_msg(item, messages)
+
+            # Testa se argumento eh um registrador valido
             if X >= len(registers):
                 messages = [
                     'Erro na chamada de módulo.',
                     f'O argumento {X} não é um registrador válido.']
                 error_msg(item, messages) 
 
+    # Extrai nome do modulo e cria o caminho para modulos
     module_name = line.split(' ')[1].split('(')[0]
     module_path_name = f'lib/{module_name}.lmm'
+
+    # Testa se arquivo do modulo existe
     if not os.path.isfile(module_path_name):
         messages = [
             'Erro na chamada de módulo.',
@@ -172,9 +183,17 @@ def analyse_module_calling(item, registers):
         error_msg(item, messages)
 
 def run_debug_mode(registers, commands, CP):
+    '''
+    Usado para mostrar detalhes do estado de operacao do programa
+    '''
     print(f'Depurando::: Registradores: {registers} --- Comando: {commands[CP]}')
 
 def verify_program_integrity(commands, registers, item):
+    '''
+    Verifica integridade do programa.
+    Todo programa ou modulo deve ter pelo menos um registrador alem do padrao.
+    Todo programa ou modulo deve ser terminado com o comando F
+    '''
     if len(registers) == 1:
         messages = [
             'Erro de integridade do programa.',
@@ -188,34 +207,77 @@ def verify_program_integrity(commands, registers, item):
 
 
 def error_msg(item, messages):
+    '''
+    Funcao para formatar e apresentar erros encontrados juntamente como o 
+    traceback quando necessario
+    '''
+
+    '''
+    Extrai dados de item contendo informacoes sobre a linha de programa
+    que causou o erro.
+    '''
     line = item.line
     line_number = item.line_number
     program_name = item.program_name
     loc = f'{program_name}: {line_number}'
+
     print(50*'-')
+
+    # Imprime a lista de traceback
     for traceback in traceback_list:
         tb_line = traceback.line
         tb_line_number = traceback.line_number
         tb_program_name = traceback.program_name
         tb_loc = f'{tb_program_name}: {tb_line_number}'
         print(f'<<< ERRO - [Traceback] - {tb_loc} >>> {tb_line}')
+
+    # Imprime localizacao e linha do erro
     print(f'<<< ERRO - {loc} >>> {line}')
+
+    # Imprime as messagens associadas ao erro
     for message in messages:
         print(f'<<< ERRO - {loc} >>> {message}')
+
     print(50*'-')
+
+    # Sai de forma silenciosa da execucao do programa
     raise SystemExit(0)
 
 def execute(program_name, args_val = []):
+    '''
+    Funcao responsavel por executar um programa ou modulo.
+    O programa principal sempre vem com args_val vazia.
+    Um modulo nunca vem com args_val vazia.
+    Essa funcao sera chamada de forma recursiva para modulos.
+    '''
     args_len = len(args_val)
+
+    # Gerando a lista program com a representacao intermediaria do programa
     program = generate_program(program_name)
+
+    # Criando a lista de registradores e de comandos a serem executados
     registers, commands = program_scanning(program, args_val)
+
+    # Executando o programa e obtendo os registradores ao final
     registers = run_program(registers, commands)
+
+    # Para modulo eh necessario retornar uma versao reduzida dos registradores.
     if args_len:
-        return registers[1:args_len+1] # Offset de 1 para deixar o elemento 0 intacto
+        # Nao inclui o registrador ZERO
+        # Nao inclui os registradores criados durante a execucao do modulo
+        # Somente os registradores passados como argumentos sao retornados
+        return registers[1:args_len+1]
     else:
+        # Aqui eh o caso de execucao do programa principal
         return None
 
 def generate_program(program_name):
+    '''
+    Essa funcao carrega o arquivo de programa ou modulo e faz um 
+    pre-processamento para limpar as linhas e eliminar linhas vazias;
+    As linhas sao transformadas em objetos da classe Line e adicionadas
+    ao program
+    '''
     program = []
     with open(program_name, 'r') as program_content:
         for ln, line in enumerate(program_content.readlines()):
@@ -227,24 +289,46 @@ def generate_program(program_name):
 
 def program_scanning(program, args_val):
     """Analisar o conteúdo de program e criar registers e commands"""
+
+    # registers sempre comeca com um registrador padrao indexado em ZERO
     registers = [0, ]
+
+    # commands sempre comeca vazia
     commands  = []
 
     for item in program:
         line = item.line
+
+        # Tokenizacao das linhas de programa
         tokens = line.split(' ')
 
+        # Criacao dos registradores
         if tokens[0] == 'R':
             if args_val:
+                '''
+                Em caso de modulos os valores dos registradores sao trazidos
+                diretamente pelos argumentos e transferidos um a um para os 
+                registradores do modulo que sao inicializados via argumentos
+                '''
                 val = args_val.pop(0)
             else:
+                '''
+                Para o programa principal os registradores sao verificados
+                atraves de register_value()
+                '''
                 val = register_value(item, registers)
+
             registers.append(val)
 
+        # Identificando chamada de modulo
         if tokens[0] == '.':
+            # Eh necessario verificar se a chamada de modulo esta correta.
             analyse_module_calling(item, registers)
             commands.append(Command(item))
 
+        # Comandos de operacoes sao identificados aqui
+        # TODO: Remover necessidade de verificar len de tokens aqui
+        # TODO: Unificar commands.append(Command(item))
         if len(tokens[0]) == 1 and tokens[0] in '+-PCEF':
             commands.append(Command(item))
 
@@ -253,50 +337,82 @@ def program_scanning(program, args_val):
     return registers, commands
 
 def run_module(module, registers):
+    '''
+    Chamada quando modulo eh encontrado em run_program()
+    '''
+
+    # Extrai argumentos da chamada de modulo
     args = module.split('(')[1].split(')')[0].split(',')
+
+    # Indentifica os valores dos registradores associados aos argumentos
     args_val = [registers[int(arg)] for arg in args]
 
+    # Extrai nome do modulo e cria caminho para abrir arquivo
     module_name = module.split('(')[0]
     module_path_name = f'lib/{module_name}.lmm'
 
+    # Executa modulo de forma recursiva recebendo os novos valores de registradores
     new_reg = execute(module_path_name, args_val)
+
+    # Substitui os valores antigos nos registradores por novos calculados
     for arg, reg in zip(args, new_reg):
         registers[int(arg)] = reg
 
     return registers
 
 def run_program(registers, commands):
+    '''
+    Interpretador do programa ou modulo
+    '''
+
+    # Inicializa o contador do programa
     CP = 0
+
+    # Loop ate o comando F ser encontrado
     while True:
         if debugging_mode:
             run_debug_mode(registers, commands, CP)
 
+        # Identifica o comando a ser interpretado
         command = commands[CP]
         ope = command.operation
         arg = command.argument
         lab = command.label
 
+        # Operacao de finalizar a execucao
         if ope == 'F':
             break
+
+        # Operacao de incremento
         elif ope =='+':
             registers[arg] += 1
             CP += 1
+
+        # Operacao de decremento
         elif ope =='-':
             registers[arg] -= 1
             CP += 1
+
+        # Operacao de pular
         elif ope =='P':
             for line, cmd in enumerate(commands):
                 if cmd.label == arg:
                     CP = line
                     break
+
+        # Operacao de comparar
         elif ope == 'C':
             if registers[arg] > 0:
                 CP += 1
             else:
                 CP += 2
+
+        # Operacao de escrever
         elif ope == 'E':
             print(f'Registrador {arg}: {registers[arg]}')
             CP += 1
+
+        # Operacao de chamar modulo
         elif ope == '.':
             traceback_list.append(command.trace)
             registers = run_module(arg, registers)
@@ -305,7 +421,14 @@ def run_program(registers, commands):
 
     return registers
 
+# Define modo de depuracao
 debugging_mode = False
+
+# Inicializa lista de traceback
 traceback_list = []
+
+# Caminho para o programa principal a ser executado
 program_name = 'examples/somar.lmp'
+
+# Entrypoint para execucao do programa
 execute(program_name)
